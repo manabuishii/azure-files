bc11dbce3d9ee67a08505a486684c89aa2765f31
Manabu ISHII <manabu.ishii.rb@gmail.com>
2017-01-31 19:45:06 +0900

    Add azure vm-sshkey-script

---
+++ vm-sshkey-script/azuredeploy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# This script can be found on https://github.com/manabuishii/azure-files/azuredeploy.sh
+# This script is part of azure deploy ARM template
+# This script assumes the Linux distribution to be Ubuntu (or at least have apt-get support)
+
+# Basic info
+date > /tmp/azuredeploy.log.$$ 2>&1
+whoami >> /tmp/azuredeploy.log.$$ 2>&1
+echo $@ >> /tmp/azuredeploy.log.$$ 2>&1
+
+echo "Hello [$1] world" > /tmp/helloworld.txt.$$ 2>&1
+exit 0

a2b5509cc5ee40a507bd5edb57b28c6fd45303fc
Manabu ISHII <manabu.ishii.rb@gmail.com>
2017-02-02 20:22:08 +0900

    Install ChefDK 1.2.20

--- leader_followers/azuredeploy.sh
+++ leader_followers/azuredeploy.sh
@@ -10,4 +10,5 @@ whoami >> /tmp/azuredeploy.log.$$ 2>&1
 echo $@ >> /tmp/azuredeploy.log.$$ 2>&1
 
 echo "Hello [$1] world" > /tmp/helloworld.txt.$$ 2>&1
+curl -L https://www.opscode.com/chef/install.sh | sudo bash -s -- -P chefdk -v 1.2.20 > /tmp/chef.txt.$$ 2>&1
 exit 0

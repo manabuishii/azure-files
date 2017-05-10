fa9c1200db1d39c76d4accea5beb163c16083a86
Manabu ISHII <manabu.ishii.rb@gmail.com>
2017-05-10 11:43:51 +0900

    Change variable names

--- scripts_for_setup/galaxy_SGE/master_script.sh
+++ scripts_for_setup/galaxy_SGE/master_script.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # setupuser
 if [ "$#" -ne 2 ]; then
-  echo "Usage: $0 USERNAME QMASTERHOST" >> /tmp/azuredeploy.log.$$
+  echo "Usage: $0 NEWUSER MASTER_NAME" >> /tmp/azuredeploy.log.$$
   exit 1
 fi
-DOCKERUSER=$1
-QMASTERHOST=$2
+NEWUSER=$1
+MASTER_NAME=$2
 # Pull Docker image
 docker pull myoshimura080822/galaxy_in_docker_bitwf:160607
 # Galaxy Singleuser
-mkdir -p /home/${DOCKERUSER]/work/github/
-cd /home/${DOCKERUSER]/work/github/
+mkdir -p /home/${NEWUSER]/work/github/
+cd /home/${NEWUSER]/work/github/
 git clone https://github.com/manabuishii/docker-galaxy-singleuser.git
 cd docker-galaxy-singleuser
 curl -s -o ./job_conf.xml.sge.docker https://raw.githubusercontent.com/manabuishii/azure-files/master/scripts_for_setup/galaxy_SGE/job_conf.sge.docker
